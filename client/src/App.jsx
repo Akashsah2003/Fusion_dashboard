1973ce3fb4df8fd49e2ba97526203a5b5640a873
Sarayu-15 <147972889+Sarayu-15@users.noreply.github.com>
2024-08-21 22:36:52 +0530

    Profile.js ,App.js,assets are modified (#2)
    Co-authored-by: AKASH KUMAR SAH <73382164+Akashsah2003@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -46,13 +46,11 @@ function App() {
             <div className='flex w-3/4 overflow-y-scroll'>
               {/* Main Pages Here */}
               <Dashboard />
-            </div>
-            <div className='flex flex-col w-1/4 bg-slate-400 h-full justify-around items-center'>
-              {/* Profile section right side bar here */}
-              <Profile />
-              <Announcements />
-            </div>
           </div>
+          <div className="flex flex-col w-1/4 h-full justify-between">
+                        <Profile />
+                        <Announcements />
+                    </div>
         </div>
       </div>
     </>
